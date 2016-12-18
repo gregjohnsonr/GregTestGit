40758f9e6d1df13167c3ad304728726e6add0b74
greg <greg@haf8c>
2016-12-18 04:03:35 +0000

    Displaying the parameters....

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -21,6 +21,12 @@ using namespace std;
  */
 int main(int argc, char** argv) {
     cout << "Hello World\n";
+    for (int i=1; i<argc; i++) {
+        cout << "Parameter " << i << " is :" << argv[i] << "\n";
+    }
+    if (argc < 2) {
+        cout << "** No Parameters given **";
+    }
     return 0;
 }
 
